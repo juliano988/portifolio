091696184ffd7a187d6dbed3dba9e3c8093b189d
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-03-09 12:36:34 -0300

    Modal fcc criado

--- addClasses.js
+++ addClasses.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
 
-  $("#link1").addClass("btn btn-primary");
-  $("#link2").addClass("btn btn-warning");
-  $("#link3").addClass("btn btn-dark");
+  $("#linkFcc1").addClass("btn btn-primary");
+  $("#linkFcc2").addClass("btn btn-warning");
+  $("#linkFcc3").addClass("btn btn-danger");
   $("#link4").addClass("btn btn-danger");
   $("#arrow").addClass("btn btn-dark");
 
--- jsCode.js
+++ jsCode.js
@@ -10,23 +10,25 @@ $(document).ready(function () {
     $('#jf-name').click(function () {
         document.getElementById('tippy-2').setAttribute('style', 'display: none');
     });
+
     //-----------------------------
 
-    $('#link1').click(function () {
+    $('#linkFcc1').click(function () {
+        document.getElementById('fccCurriculumProjects').setAttribute('style', 'display: none');
         document.getElementById('sec-2').setAttribute('style', 'display: block');
         document.getElementById('sec-3').setAttribute('style', 'display: none');
         document.getElementById('sec-4').setAttribute('style', 'display: none');
         document.getElementById('sec-5').setAttribute('style', 'display: none');
     });
 
-    $('#link2').click(function () {
+    $('#linkFcc2').click(function () {
         document.getElementById('sec-2').setAttribute('style', 'display: none');
         document.getElementById('sec-3').setAttribute('style', 'display: block');
         document.getElementById('sec-4').setAttribute('style', 'display: none');
         document.getElementById('sec-5').setAttribute('style', 'display: none');
     });
 
-    $('#link3').click(function () {
+    $('#linkFcc3').click(function () {
         document.getElementById('sec-2').setAttribute('style', 'display: none');
         document.getElementById('sec-3').setAttribute('style', 'display: none');
         document.getElementById('sec-4').setAttribute('style', 'display: block');
@@ -40,7 +42,7 @@ $(document).ready(function () {
         document.getElementById('sec-5').setAttribute('style', 'display: block');
     });
 
-    $('#link1 , #link2 , #link3 , #link4').click(function () {
+    $('#linkFcc1 , #linkFcc2 , #linkFcc3 , #link4').click(function () {
         setTimeout(function () { document.getElementById('arrow').setAttribute('style', '  opacity: 1; transform: scale(1);') }, 500);
         setTimeout(function () { document.getElementById('tippy-1').setAttribute('style', '  opacity: 1; transform: scale(1);') }, 500);
         setTimeout(function () { document.getElementById('sec-1').setAttribute('style', 'transform: scale(0);') }, 500);
@@ -50,5 +52,6 @@ $(document).ready(function () {
         document.getElementById('sec-1').setAttribute('style', 'transform: scale(1);');
         setTimeout(function () { document.getElementById('arrow').setAttribute('style', '  opacity: 0; transform: scale(0);') }, 500);
         setTimeout(function () { document.getElementById('tippy-1').setAttribute('style', '  opacity: 0; transform: scale(0);') }, 500);
+        document.getElementById('fccCurriculumProjects').setAttribute('style', 'display: initial');
     })
 });
